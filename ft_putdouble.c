5e8c732f6eeb6c36752dcc50cc51c7bab4c9b32e
Theo SEGUIER <tseguier@e2r12p17.42.fr>
2014-10-01 00:05:23 +0200

    erreurcompilo

--- ft_putdouble.c
+++ ft_putdouble.c
@@ -1,12 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putdouble.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tseguier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/09/29 21:01:15 by tseguier          #+#    #+#             */
+/*   Updated: 2014/09/30 23:58:19 by tseguier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include "libft.h"
 
-//TODO: width specifier,fill char
-
-static int		ft_printradix(double nb, int prec, char *buf)
+static int	ft_printradix(double nb, int prec, char *buf)
 {
 	char	digit;
-	int	size;
+	int		size;
 
 	size = prec + 1;
 	*buf++ = RADIX_CHAR;
@@ -14,8 +24,7 @@ static int		ft_printradix(double nb, int prec, char *buf)
 	{
 		if (nb != 0)
 		{
-			nb *= 10;
-			digit = (char)nb;
+			digit = (char)(nb *= 10);
 			nb -= (double)digit;
 			*buf++ = digit + '0';
 		}
@@ -26,12 +35,12 @@ static int		ft_printradix(double nb, int prec, char *buf)
 	return (size);
 }
 
-int		ft_putdouble(double nb, int prec, int sign)
+int			ft_putdouble(double nb, int prec, int sign)
 {
-	char	buf[MAX_DBLSIZE];
-	int	size;
-	double	mask;
-	char	digit;
+	char		buf[MAX_DBLSIZE];
+	int			size;
+	double		mask;
+	char		digit;
 
 	size = 0;
 	mask = 1.0;
@@ -39,7 +48,7 @@ int		ft_putdouble(double nb, int prec, int sign)
 	{
 		buf[size++] = nb < 0 ? '-' : '+';
 		if (nb < 0)
-		    nb = 0 - nb;
+			nb = 0 - nb;
 	}
 	while (nb / mask > 10.0)
 		mask *= 10.0;
--- ft_putldouble.c
+++ ft_putldouble.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putldouble.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tseguier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/09/29 20:53:47 by tseguier          #+#    #+#             */
+/*   Updated: 2014/10/01 00:02:53 by tseguier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include "libft.h"
 
-//TODO: width specifier, fill char
-static int		ft_printradix(long double nb, int prec, char *buf)
+static int	ft_printradix(long double nb, int prec, char *buf)
 {
 	char	digit;
-	int	size;
+	int		size;
 
 	size = prec + 1;
 	*buf++ = RADIX_CHAR;
@@ -13,8 +24,7 @@ static int		ft_printradix(long double nb, int prec, char *buf)
 	{
 		if (nb != 0)
 		{
-			nb *= 10;
-			digit = (char)nb;
+			digit = (char)(nb *= 10);
 			nb -= (long double)digit;
 			*buf++ = digit + '0';
 		}
@@ -27,10 +37,10 @@ static int		ft_printradix(long double nb, int prec, char *buf)
 
 int			ft_putldouble(long double nb, int prec, int sign)
 {
-	char	buf[MAX_DBLSIZE];
-	int	size;
-	long double	mask;
-	char	digit;
+	char			buf[MAX_DBLSIZE];
+	int				size;
+	long double		mask;
+	char			digit;
 
 	size = 0;
 	mask = 1.0;
@@ -38,7 +48,7 @@ int			ft_putldouble(long double nb, int prec, int sign)
 	{
 		buf[size++] = nb < 0 ? '-' : '+';
 		if (nb < 0)
-		    nb = 0 - nb;
+			nb = 0 - nb;
 	}
 	while (nb / mask > 10.0)
 		mask *= 10.0;
@@ -51,5 +61,5 @@ int			ft_putldouble(long double nb, int prec, int sign)
 	if (prec > 0)
 		size += ft_printradix(nb, prec, buf + size);
 	buf[size] = '\0';
-	return (write(1, buf, size), ft_putchar('\n'));
+	return (write(1, buf, size));
 }
